5794a1de3a252e57369187c4e8a050fa7e0104bf
EnriqueEsquia1212 <EnriqueEsquia1212@outlook.es>
2024-07-11 08:51:36 -0600

    prueba

--- SysInventario.EntidadDeNegocio/Rol.cs
+++ SysInventario.EntidadDeNegocio/Rol.cs
@@ -13,6 +13,7 @@ public partial class Rol
     [Required(ErrorMessage = "Nombre es obligatorio")]
     [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
     public string Nombre { get; set; }
+    //Enrique
     [NotMapped]
     public int Top_Aux { get; set; }
     public List<Usuario> Usuario { get; set; }
